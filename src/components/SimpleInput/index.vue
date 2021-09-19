ee0f311670386062ffc0cffd206959a91f1f108d
Aphirat Nimanussonkul <Tata@odds.team>
2021-09-19 09:27:59 +0700

    create simple input

---
+++ src/components/SimpleInput/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="flex flex-col w-full gap-y-2">
+    <label class="text-sm">{{ label }}</label>
+    <input
+      class="focus:outline-none border-b border-primary-400"
+      v-model="model"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "SimpleInput",
+  props: {
+    label: String,
+    value: {
+      type: String,
+      required: true,
+    },
+  },
+  computed: {
+    model: {
+      get() {
+        return this.value;
+      },
+      set(value: String | number) {
+        this.$emit("typing", value);
+      },
+    },
+  },
+});
+</script>
--- src/views/Information.vue
+++ src/views/Information.vue
@@ -5,8 +5,15 @@
       <h1 class="font-bold text-2xl mt-2">Who are you?</h1>
       <p class="text-gray-400 text-md">Tell us about yourself</p>
       <GenderSelection :selected="gender" :onSelect="onSelectGender" />
+      <div class="mt-4">
+        <SimpleInput
+          :label="`Your name`"
+          :value="name"
+          v-on:typing="onChange"
+        />
+      </div>
     </div>
-    <MainButton label="Let's begin" />
+    <MainButton label="Let's begin" @click="onRouteToHomePage" />
   </div>
 </template>
 
@@ -16,14 +23,16 @@ import router from "@/router";
 import MainButton from "../components/MainButton/index.vue";
 import ArrowLeftButton from "../components/ArrowLeftButton/index.vue";
 import GenderSelection from "../components/GenderSelection/selector.vue";
+import SimpleInput from "../components/SimpleInput/index.vue";
 import { Gender } from "@/enum/gender";
 
 export default defineComponent({
   name: "Information",
-  components: { MainButton, ArrowLeftButton, GenderSelection },
+  components: { MainButton, ArrowLeftButton, GenderSelection, SimpleInput },
   data() {
     return {
       gender: "",
+      name: "",
     };
   },
   methods: {
@@ -32,6 +41,12 @@ export default defineComponent({
     },
     onSelectGender(gender: Gender) {
       this.gender = gender;
+    },
+    onChange(data: string) {
+      this.name = data;
+    },
+    onRouteToHomePage() {
+      console.log(this.name);
     },
   },
 });
